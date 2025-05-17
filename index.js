f06a62a2b29fefa518718af0444951c8b400a6cc
Precious2712 <oluwanifemimoonlight@gmail.com>
2025-05-16 16:18:36 -0800

    deleted some code

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ require('dotenv').config();
 
 const express = require('express');
 const cors = require('cors');
-// const cookieParser = require('cookie-parser')
 const mongoose = require('mongoose');
 
 const testRequest = require('./src/route/request');
@@ -16,7 +15,6 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
-// app.use(cookieParser());
 
 app.use('/api/v1', testRequest);
 app.use('/api/v2', signup);
